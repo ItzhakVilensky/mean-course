ab0cce56def1348212a596db83d43d60a06f6aa9
Itzhak Vilensky <itzhak.vil@gmail.com>
2022-04-15 19:11:30 +0300

    * lesson 24 happy pesah

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -6,7 +6,9 @@ import { Component } from "@angular/core";
     styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-    newPost = '';
+
+    enteredContent = '';
+    enteredTitle = '';
 
     onAddPost() {
 
