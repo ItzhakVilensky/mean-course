3d4c75e801121cffd1c1822140fdef7342320981
Itzhak Vilensky <itzhak.vil@gmail.com>
2022-04-15 17:44:26 +0300

    Add style to card, textarea, buttonpost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -16,7 +17,8 @@ const angularModules = [
 const materialModules = [
   MatInputModule,
   MatFormFieldModule,
-  MatCardModule
+  MatCardModule,
+  MatButtonModule
 ];
 
 @NgModule({
