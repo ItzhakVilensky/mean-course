92baba8c84cdfc29296fd06bcf499377160d93b3
Itzhak Vilensky <itzhak.vil@gmail.com>
2022-04-15 17:34:38 +0300

    * create mat-card + mat-form-field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+const angularModules = [
+  BrowserModule,
+  BrowserAnimationsModule,
+];
+const materialModules = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatCardModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    angularModules,
+    materialModules
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
