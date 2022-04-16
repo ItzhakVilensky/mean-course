3b1d8bea71f8166c09152697d8407c3214b90f2a
Itzhak Vilensky <itzhak.vil@gmail.com>
2022-04-16 14:29:13 +0300

    * Call getPost via subject and subscription that does not work

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { Post } from "../post.model";
 import { PostService } from "../post.service";
 
@@ -7,10 +8,24 @@ import { PostService } from "../post.service";
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
 
-    @Input() posts: Post[] = [];
+    posts: Post[] = [];
+    private postSubscription = new Subscription();
 
     constructor(public postService: PostService) {
+    }
+
+    ngOnInit() {
+        this.posts = this.postService.getPosts();
+
+        this.postSubscription = this.postService.getPostUpdateListener
+        .subscribe((posts: Post[]) => {
+            this.posts = posts;
+        });
+    }
+
+    ngOnDestroy() {
+        this.postSubscription.unsubscribe();
     }
 }
