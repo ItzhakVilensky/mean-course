1d6382cd8a736f0a3f9a96e5da190e2b80bcdce9
Itzhak Vilensky <itzhak.vil@gmail.com>
2022-04-18 19:18:29 +0300

    * configure app.js to fetch hard coded posts with 200 ok status

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const { json } = require('stream/consumers');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware in a chain');
-    next();
-});
+// app.use((req, res, next) => {
+//     next();
+// });
 
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+    // res.send('Hello from express!');
+    const posts = [{
+        id: 'id1',
+        title: 'Server side post',
+        content: 'server sidce content'
+    },
+    {
+        id: 'id2',
+        title: 'Another server side post',
+        content: 'Another server sidce content'
+    }];
+    res.status(200).json({
+        message: 'server message for complexity',
+        posts: posts
+    });
 });
 
 module.exports = app;
